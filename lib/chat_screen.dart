414db000a372b9971281cad4a827c2b394206f32
divyang-mobio <107481877+divyang-mobio@users.noreply.github.com>
2022-09-28 18:14:34 +0530

    chat implement

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
 import 'package:flutter_styled_toast/flutter_styled_toast.dart';
-
 import 'bloc/message_bloc.dart';
 import 'message_model.dart';
 
